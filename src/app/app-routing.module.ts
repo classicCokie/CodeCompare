c636862ef3e798201e9ce1aa7d9a2ac711a52069
Yannic <yannic@Yannics-MacBook-Pro.local>
2018-08-11 10:38:20 +1000

    delpoy that

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchListComponent } from './search-list/search-list.component';
-import { PersonProfileComponent } from './person-profile/person-profile.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
@@ -12,7 +11,6 @@ import { ReviewCodeComponent } from './review-code/review-code.component';
 const routes: Routes = [
   { path: '', redirectTo: '/search', pathMatch: 'full' },
   { path: 'search', component: SearchListComponent },
-  { path: 'profiel', component: PersonProfileComponent},
   { path: 'login', component: LoginComponent},
   { path: 'anmelden', component: SignUpComponent},
   { path: 'profiel_erstellen', component: CreateProfileComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { SearchListComponent } from './search-list/search-list.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { PersonProfileComponent } from './person-profile/person-profile.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
@@ -20,7 +18,6 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     SearchListComponent,
-    PersonProfileComponent,
     LoginComponent,
     SignUpComponent,
     CreateProfileComponent,
